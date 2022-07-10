bb46cd2640d71181d2c67b98c8b3985c1f87725e
nash1111 <ken1251213@gmail.com>
2022-07-10 20:27:12 +0900

    Add os.EOL for windows ci

--- tests/readASCIISTL.spec.ts
+++ tests/readASCIISTL.spec.ts
@@ -1,6 +1,8 @@
+import * as os from 'os';
 import { describe, expect, it } from "vitest";
 import { readASCIISTL, readASCIISTLFromFilePath } from "../lib/readASCIISTL";
 const fs = require("fs");
+const EOL: string = os.EOL;
 
 
 const sampleASCIISTLPath = "./samples/ballascii.stl"
@@ -15,17 +17,16 @@ describe("readASCIISTL", () => {
   });
 })
 
-const sampleASCIISTL = `solid Visualization Toolkit generated SLA File
- facet normal 0.062746412054374687 -0.037362288100243335 -0.99732990890788853
-  outer loop
-   vertex 0.60154032707214355 0.037101738154888153 -4.9635443687438965
-   vertex 0.62666618824005127 -1.5348894778271024e-16 -4.960573673248291
-   vertex 3.0616169991140216e-16 -7.4987987861059711e-32 -5
-  endloop
- endfacet
-endsolid
+const sampleASCIISTL: string = 'solid Visualization Toolkit generated SLA File' + EOL +
+  ' facet normal 0.062746412054374687 -0.037362288100243335 -0.99732990890788853' + EOL +
+  '  outer loop' + EOL +
+  '   vertex 0.60154032707214355 0.037101738154888153 -4.9635443687438965' + EOL +
+  '   vertex 0.62666618824005127 -1.5348894778271024e-16 -4.960573673248291' + EOL +
+  '   vertex 3.0616169991140216e-16 -7.4987987861059711e-32 -5' + EOL +
+  '  endloop' + EOL +
+  ' endfacet' + EOL +
+  'endsolid' + EOL
 
-`
 
 describe("readASCIISTL", () => {
   it("test readASCIISTL", () => {
@@ -34,5 +35,11 @@ describe("readASCIISTL", () => {
 
     // test get name
     expect(result.name).toStrictEqual(expectedName);
+    console.log("aaaa" + sampleASCIISTL)
+    expect(result.normalVectors).toStrictEqual([{
+      x: 0.06274641205437469,
+      y: -0.037362288100243335,
+      z: -0.9973299089078885
+    }]);
   });
 })
